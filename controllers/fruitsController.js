f30ca4d73544ce2ba1888d646d2e473f3195b64f
Parth Patel <parth@Parths-MacBook-Pro.local>
2021-08-02 11:34:57 +0100

    built api

--- controllers/fruitsController.js
+++ controllers/fruitsController.js
@@ -2,7 +2,15 @@ const express = require('express');
 const fruits = require('../data/fruits');
 const router = express.Router();
 
-guessCount = 0;
+const getAnswer = () =>{
+    let idx = Math.floor(Math.random()*fruits.length);
+    return fruits[idx];
+}
+
+const answer = getAnswer();
+
+let isClueRequested = false;
+let guessCount = 0;
 
 router.get('/', (req,res) =>{
     res.send('Hello and welcome to Fruit Ninjas!')
@@ -11,11 +19,30 @@ router.get('/', (req,res) =>{
 router.get('/guess/:userguess', (req, res)=>{
     const guess = req.params.userguess;
     if(isCorrect(guess, answer)){
-        res.send(`Correct!`)
+        res.send(`Correct! Your score is ${isClueRequested ? guessCount *2 : guessCount}. You have won a box of ${guess}s`)
     } else{
         guessCount++;
-        res.send(`Incorrect!`);
+        res.send(`Incorrect! ${guessCount} guesses`);
     }
 })
+
+router.get('/clue', (req, res)=>{
+    isClueRequested = true;
+    let str = JSON.stringify(answer);
+    let arr = str.split(':');
+    res.send(arr[1].slice(1,-2));
+})
+
+function getKey(str){
+    result = JSON.stringify(str);
+    let arr = result.split(':');
+    return arr[0].slice(2,-1);
+}
+
+function isCorrect(guess, answer){
+    return guess == getKey(answer) ? true : false;
+}
+
+
 
 module.exports = router;
